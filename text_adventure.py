5addb5a7c47f759c591699f1ad4958fcd7534876
Adam Mohammed <adammoham@gmail.com>
2016-08-09 20:30:11 -0400

    Added a dictionary to have a rudimentary player class

--- text_adventure.py
+++ text_adventure.py
@@ -13,12 +13,17 @@ def create_character():
     print "Welcome to Cralohines. You crash landed on this densely forested planet"
     print "You slowly get up from your horrific landing and look around"
     print "Somehow nothing is broken but your head is bleeding and you can't remember much"
+    # To avoid having a class with just __init__ Player will be a dictionary
+    # Player will have a name, age, race, health, and four item slots
+    player = {"name": '', "age": 0, "race": "human", "item_slot_1": None,
+                "item_slot_2": None, "item_slot_3": None, "item_slot_4": None}
     name = str(raw_input("Whispers come from the north, what are they saying? "))
+    player["name"] = name
     # you get input with raw_input("prompt")
     # you usually want to cast it to the type you want, e.g. int, str, etc.
-    return name
+    return player
 
 if __name__ == "__main__":
     it_begins()
-    name = create_character()
+    player = create_character()
     # Do something with the name now
